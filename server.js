bee186be0ad5c836be83be6efb3837f2aa99dca0
clementsamuel <clement290695@gmail.com>
2016-09-30 20:17:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/article-two', function(req, res) {
 var counter=0;
 app.get('/counter',function(req, res) {
     counter=counter+1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
 });
 
 app.get('/article-three', function(req, res) {
